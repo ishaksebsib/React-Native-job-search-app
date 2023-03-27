5c59fb155c73755702d54aae88f37684fc616608
ishaksebsib <ishaksebsib@gmail.com>
2023-03-27 17:11:42 +0300

    navigation bar style done

--- app/index.js
+++ app/index.js
@@ -19,10 +19,14 @@ const Home = () => {
           headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension="60%"
+              handlePress={() => console.log("hiiii")}
+            />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: "",
         }}

5cf11dd619039b791fec5eccadaed1f7d79c30fb
ishaksebsib <ishaksebsib@gmail.com>
2023-03-27 16:57:53 +0300

    basic screen and folder structure setup

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { useState } from "react";
+import { Stack } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+
+import {
+  Welcome,
+  ScreenHeaderBtn,
+  Popularjobs,
+  Nearbyjobs,
+} from "../components";
 
 const Home = () => {
   return (
-    <View>
-      <Text> Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

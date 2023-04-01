b459cf32405d9a8edbf67a177b506bdedc6256d0
ishaksebsib <ishaksebsib@gmail.com>
2023-04-01 14:18:35 +0300

    popular jobs component setup

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,9 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   // Ensure any route can link back to `/`
@@ -6,5 +11,18 @@ export const unstable_settings = {
 };
 
 export default Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
   return <Stack />;
 };
